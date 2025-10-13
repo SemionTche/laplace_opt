57c63e2081cd11c3cff47a1bd449900e14679269
Semion <semion.tchetovsky@gmail.com>
2025-10-13 16:00:49 +0200

    input structure

---
+++ inputs/input_structure.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+from typing import Sequence
+
+def check_bounds_format(bounds: Sequence[float]):
+    """
+    checks if the boundaries provided respect the format:
+        length = 2 and minimum in first position.
+    """
+    if len(bounds) != 2 or bounds[0] > bounds[1]:
+        raise ValueError("bounds must have exactly 2 elements: (min, max)")
+
+class InputStructure(ABC):
+    
+    def __init__(self, name: str, bounds: Sequence[float]):
+        check_bounds_format(bounds)
+
+        self._name = name
+        self._bounds = bounds
+    
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def bounds(self) -> Sequence[float]:
+        return self._bounds
+
+    @abstractmethod
+    def get_position(self) -> None:
+        """
+        return the current position of the input.
+        Must be implemented by subclasses.
+        """
+        pass
+
+    def set_bounds(self, new_bounds: Sequence[float]) -> None:
+        check_bounds_format(new_bounds)
+        self._bounds = new_bounds
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__}(name='{self.name}', bounds={self.bounds})>"
