43bf26f86cff4212696c9834744559aabbf1bc7f
Semion <semion.tchetovsky@gmail.com>
2026-02-06 14:51:24 +0100

    Single acq func

--- laplace_opt/model_construction/acquisitions/acquisition_qlognei.py
+++ laplace_opt/model_construction/acquisitions/acquisition_qlognei.py
@@ -1,21 +1,19 @@
 import torch
-from botorch.acquisition.multi_objective.logei import (
-    qLogNoisyExpectedHypervolumeImprovement,
-)
+from botorch.acquisition.logei import qLogNoisyExpectedImprovement
 from botorch.sampling import SobolQMCNormalSampler
 
 from laplace_opt.core.optimizerContext import OptimizationContext
 from laplace_opt.model_construction.acquisitions.acquisition_structure import AcquisitionStructure
 
 
-class TestAcq(AcquisitionStructure):
-    display_name = "TEST ACQ"
+class qLogNEI(AcquisitionStructure):
+    display_name = "qLogNEI"
 
     parameters = {
         "mc_samples": {
             "type": int, 
             "default": 128,
-            "description": "",
+            "description": "Monte Carlo sample size",
             "label": "mc_samples"
         },
     }
@@ -25,27 +23,15 @@ class TestAcq(AcquisitionStructure):
         model,
         context: OptimizationContext,
         mc_samples: int,
-        alpha: float,
         **kwargs,
     ):
         sampler = SobolQMCNormalSampler(
             sample_shape=torch.Size([mc_samples])
         )
 
-        return qLogNoisyExpectedHypervolumeImprovement(
+        return qLogNoisyExpectedImprovement(
             model=model,
-            ref_point=context.compute_ref_point(),
             X_baseline=context.get_X_baseline_normalized(),
             sampler=sampler,
-            alpha=alpha,
             **kwargs
         )
-
-
-    # def get_sampler(self, mc_samples: int):
-    #     """
-    #     Return a sampler compatible with ModelListGP.
-    #     """
-    #     return SobolQMCNormalSampler(
-    #         sample_shape=torch.Size([mc_samples])
-    #     )
