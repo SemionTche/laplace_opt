6619fbbadbc5dd034d342a7fb38b52aa4911e807
Semion <semion.tchetovsky@gmail.com>
2026-08-06 19:10:37 +0200

    Create __init__.py

---
+++ benchmark/metrics/__init__.py
@@ -0,0 +1,45 @@
+from .metric import Metric
+
+
+from .regret import (
+    SimpleRegret, AreaUnderRegretCurve
+)
+from .time_to_epsilon import TimeToEpsilon
+from .distance_to_optimum import DistanceToOptimum
+
+from .loo_rmse import LOORMSE
+from .loo_nlpd import LOONLPD
+from .coverage import Coverage
+
+from .contractions import (
+    FinalVarianceContraction,
+    FinalEntropyContraction,
+    HalfLifeVarianceContraction,
+    HalfLifeEntropyContraction,
+)
+
+from .lengthscale_stats import LengthscaleMean, LengthscaleStd
+from .noise_stats import NoiseMean, NoiseStd
+
+
+METRICS: dict[str, Metric] = {
+    "AreaUnderRegretCurve": AreaUnderRegretCurve(),
+    "SimpleRegret": SimpleRegret(),
+    "TimeToEpsilon": TimeToEpsilon(),
+    "DistanceToOptimum": DistanceToOptimum(),
+
+    "LOORMSE": LOORMSE(),
+    "LOONLPD": LOONLPD(),
+    "Covarage": Coverage(),
+
+    "FinalVarianceContraction": FinalVarianceContraction(),
+    "FinalEntropyContraction": FinalEntropyContraction(),
+
+    "HalfLifeVarianceContraction": HalfLifeVarianceContraction(),
+    "HalfLifeEntropyContraction": HalfLifeEntropyContraction(),
+
+    "LengthscaleMean": LengthscaleMean(),
+    "LengthscaleStd": LengthscaleStd(),
+    "NoiseMean": NoiseMean(),
+    "NoiseStd": NoiseStd(),
+}
